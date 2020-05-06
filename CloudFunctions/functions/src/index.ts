96a7dcfb1f3c44153be2d903bd8aa90938690b15
= <=>
2020-05-07 02:10:39 +0530

    Cloud function added, testing not done

---
+++ CloudFunctions/functions/src/index.ts
@@ -0,0 +1,48 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp()
+
+export const onNewTransaction = 
+functions.firestore.document('user/{userId}/pending_transaction/transaction')
+.onUpdate(async (change) => {
+
+    try {
+
+        const transactionObject = change.after.data()
+
+        const fromDocumentRef = (<admin.firestore.DocumentReference> transactionObject?.from)
+        const toDocumentRef = (<admin.firestore.DocumentReference> transactionObject?.to)
+
+        const transaction_promises = []
+
+        const add_transaction_in_from = fromDocumentRef
+        .collection('transaction').doc(String(transactionObject?.id)).set(<Object> transactionObject);
+        transaction_promises.push(add_transaction_in_from)
+
+        const add_transaction_in_to = toDocumentRef
+        .collection('transaction').doc(String(transactionObject?.id)).set(<Object> transactionObject)
+        transaction_promises.push(add_transaction_in_to)
+
+        const result = await Promise.all(transaction_promises)
+
+        const update_recent_trnsaction_promises = []
+
+        const fromTransactionRef = fromDocumentRef.collection('transaction').doc(transactionObject?.id)
+        const toTransactionRef = toDocumentRef.collection('transaction').doc(transactionObject?.id)
+
+        const add_recent_ransaction_in_from = fromDocumentRef
+        .collection('wallet').doc('wallet').update('lastTransaction', fromTransactionRef)
+        update_recent_trnsaction_promises.push(add_recent_ransaction_in_from)
+
+        const add_recent_ransaction_in_to = toDocumentRef
+        .collection('wallet').doc('wallet').update('lastTransaction', toTransactionRef)
+        update_recent_trnsaction_promises.push(add_recent_ransaction_in_to)
+
+        Promise.all(update_recent_trnsaction_promises)
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
--- app/src/main/java/com/visionio/sabpay/payment/PayActivity.java
+++ app/src/main/java/com/visionio/sabpay/payment/PayActivity.java
@@ -219,7 +219,7 @@ public class PayActivity extends AppCompatActivity {
             }
         });
     }
-    
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
