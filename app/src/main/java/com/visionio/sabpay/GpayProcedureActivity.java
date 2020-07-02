6a65d3f24b3f5692dc3d4f38fa658aed7b5a3e1b
sakshi.172015 <sakshi.172015@gmail.com>
2020-07-03 03:26:54 +0530

    gpay directions

---
+++ app/src/main/java/com/visionio/sabpay/GpayProcedureActivity.java
@@ -0,0 +1,17 @@
+package com.visionio.sabpay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class GpayProcedureActivity extends AppCompatActivity {
+
+    RelativeLayout rl_1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpay_procedure);
+    }
+}
