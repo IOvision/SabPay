dcdf830e009de4a57bff5948ace29e307d29a480
Danish Jamal <danishjamal.104@gmail.com>
2020-08-19 19:28:31 +0530

    Model sync

--- app/src/main/java/com/visionio/sabpay/models/Inventory.java
+++ app/src/main/java/com/visionio/sabpay/models/Inventory.java
@@ -2,37 +2,23 @@ package com.visionio.sabpay.models;
 
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.Exclude;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
-import com.google.gson.Gson;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Inventory implements Serializable {
 
     String id; // id of the inventory
     String name; // inventory name, can be name of the store
     List<String> images; // urls of image of store between 0 - 5
-    List<String> items;
-    DocumentReference owner;// user/id ref of owner of this inventory
-    //TODO: add phone number for payment
-    //String ownerPhone;
+    List<String> items; // list of ids of items in this inventory
+    GeoPoint location;
+    DocumentReference owner; // user/id ref of owner of this inventory
     boolean opened; // tells whether the shop/inventory is open or close
     int totalItems; // tells total number of unique items... for example it shop has 2 bananas and 3 apples so totalItems = 2.
-    //String location;
-    GeoPoint location;
 
-    public GeoPoint getLocation() {
-        return location;
-    }
-
-    public void setLocation(GeoPoint location) {
-        this.location = location;
-    }
 
     public Inventory() {
         totalItems = 0;
@@ -76,6 +62,14 @@ public class Inventory implements Serializable {
         this.items = items;
     }
 
+    public GeoPoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(GeoPoint location) {
+        this.location = location;
+    }
+
     public void setOwner(DocumentReference owner) {
         this.owner = owner;
     }
@@ -96,31 +90,6 @@ public class Inventory implements Serializable {
         this.totalItems = totalItems;
     }
 
-    public String getJson(){
-        Map<String, Object> map = new HashMap<>();
-
-        map.put("id", id);
-        map.put("name", name);
-        map.put("images", images);
-        map.put("items", items);
-        map.put("owner", owner.getId());
-        map.put("int", totalItems);
-
-        return new Gson().toJson(map);
-    }
-
-    public static Inventory formJson(String json){
-        HashMap<String, Object> map = new Gson().fromJson(json, HashMap.class);
-        Inventory i = new Inventory();
-        i.setId(map.get("id").toString());
-        i.setName(map.get("name").toString());
-        i.setImages((List<String>)map.get("images"));
-        i.setItems((List<String>)map.get("items"));
-        i.setOwner(FirebaseFirestore.getInstance().document("user/"+map.get("owner").toString()));
-        i.setTotalItems((int)((double) map.get("int")));
-        return i;
-    }
-
     @Exclude
     public void addImage(String imageUrl){
         images.add(imageUrl);
@@ -131,7 +100,8 @@ public class Inventory implements Serializable {
             if(!i.getId().equals("") &&
                     !i.getName().equals("") &&
                     i.getOwner()!=null &&
-                    i.getTotalItems()>=0 ){
+                    i.getTotalItems()>=0 &&
+                    i.getLocation()!=null){
                 return i;
             }
             return null;
@@ -146,6 +116,10 @@ public class Inventory implements Serializable {
         }
         public Builder setOwner(DocumentReference ownerRef){
             i.setOwner(ownerRef);
+            return this;
+        }
+        public Builder setLocation(GeoPoint loc){
+            i.setLocation(loc);
             return this;
         }
     }
